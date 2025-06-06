8f633013903a854c335766dd5075e843062b7cb0
AmolSontakke <118731985+amolsontakke96@users.noreply.github.com>
2025-06-06 22:21:05 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <div>
       <h1>React + Node + PostgreSQL</h1>
-      <h1> I Love React </h1>
+      <h1> i'm not reacting </h1>
       {data ? (
         <pre>{JSON.stringify(data, null, 2)}</pre>
       ) : (
